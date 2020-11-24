f31942713b712427b8f70befb4b85bd5157917c2
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2020-11-24 09:07:40 +0530

    RF Example 5

--- src/app/students/student-edit/student-edit.component.ts
+++ src/app/students/student-edit/student-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-edit',
@@ -10,23 +10,32 @@ export class StudentEditComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
   studentEditForm: FormGroup;
-  hobbies:FormArray = new FormArray([
-    new FormControl()
-  ]);
+  hobbies:FormArray;
+  Addresses:FormArray;
 
   ngOnInit(): void {
-    this.studentEditForm = this.fb.group({
-      FirstName: this.fb.control(""),
-      LastName: this.fb.control(""),
-      EmailId: this.fb.control(""),
-      MobileNo: this.fb.control(""),
-      Address: this.fb.group({
+    this.hobbies = this.fb.array([
+      this.fb.control("")
+    ]);
+
+    this.Addresses = this.fb.array([
+      this.fb.group({
         AddLine1: this.fb.control(""),
         AddLine2: this.fb.control(""),
         AddLine3: this.fb.control(""),
         City: this.fb.control(""),
         State: this.fb.control("")
-      }),
+      })
+    ])
+
+    this.studentEditForm = this.fb.group({
+      FirstName: this.fb.control("",[Validators.required,Validators.minLength(3),Validators.maxLength(12)]),
+      LastName: this.fb.control("",Validators.required),
+      EmailId: this.fb.control("",[Validators.required,Validators.email]),
+      MobileNo: this.fb.control(""),
+      TermsAndConditions:this.fb.control(true),
+      NotificationType:this.fb.control("email"),
+      Addresses:this.Addresses,
       Hobbies: this.hobbies
     });
   }
@@ -36,8 +45,18 @@ export class StudentEditComponent implements OnInit {
   }
 
   AddHobby() {
-    this.hobbies.push(new FormControl());
-    //(<FormArray>this.studentEditForm.get("Hobbies")).push(this.fb.control(""));
+    // this.hobbies.push(new FormControl());
+    (<FormArray>this.studentEditForm.get("Hobbies")).push(this.fb.control(""));
+  }
+
+  AddAddress() {
+    (<FormArray>this.studentEditForm.get("Addresses")).push(this.fb.group({
+      AddLine1: this.fb.control(""),
+      AddLine2: this.fb.control(""),
+      AddLine3: this.fb.control(""),
+      City: this.fb.control(""),
+      State: this.fb.control("")
+    }));
   }
 
 }
